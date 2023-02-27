efe4dd1f95220c395b7268f8eb79323368674362
junhkim20 <jk7048@nyu.edu>
2023-02-27 10:51:21 -0500

    Camera follows player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; 
+
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset; 
+    public Vector3 minValue, maxValue; 
+
+    void FixedUpdate(){
+        Vector3 desiredPosition = target.position + offset; 
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothPosition; 
+    }
+}
